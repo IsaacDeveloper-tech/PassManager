746e8a5e6c4d9d4bf41f01c9a24de8c0ef7367d2
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2024-06-01 19:13:25 +0200

    feat: Aplicamos todo el sistema CRUD

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './screens/HomeScreen';
+import { AddPasswordScreen } from './screens/AddPasswordScreen';
+import { EditPasswordScreen } from './screens/EditPasswordScreen';
+import { ViewPasswordScreen } from './screens/ViewPasswordScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>PassManager</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddPassword" component={AddPasswordScreen} />
+        <Stack.Screen name="EditPassword" component={EditPasswordScreen} />
+        <Stack.Screen name="ViewPassword" component={ViewPasswordScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
